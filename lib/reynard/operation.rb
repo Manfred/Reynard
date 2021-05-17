86b0e1b00b48325af37dd2926127513cd88b7c41
Manfred Stienstra <manfred@fngtps.com>
2021-05-17 17:27:33 +0200

    Explain what the Operation class does.

--- lib/reynard/operation.rb
+++ lib/reynard/operation.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class Reynard
+  # Holds the verb, request path, and node reference to an operation in the API specification.
   class Operation
     DEFAULT_MEDIA_TYPE = 'application/json'
 
