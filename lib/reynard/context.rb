b23ecc5434526cfb66ed4de30d63484eb187c686
Manfred Stienstra <manfred@fngtps.com>
2021-08-05 10:50:11 +0200

    Move responsibility for data to RequestContext.

--- lib/reynard/context.rb
+++ lib/reynard/context.rb
@@ -1,110 +1,65 @@
 # frozen_string_literal: true
 
 class Reynard
-  # Builds and keeps context for a single request to an endpoint.
+  # Exposes a public interface to build a request context.
   class Context
-    def initialize(specification:, base_url: nil, operation: nil, params: nil)
+    extend Forwardable
+    def_delegators :@request_context, :verb, :path, :full_path, :url
+
+    def initialize(specification:, request_context: nil)
       @specification = specification
-      @base_url = base_url || find_base_url
-      @operation = operation
-      @params = params ? params.transform_keys(&:to_s) : nil
-    end
-
-    def query?
-      grouped_params&.key?('query')
-    end
-
-    def query
-      Rack::Utils.build_query(grouped_params['query']) if query?
-    end
-
-    def path
-      return unless @operation
-
-      grouped_params.fetch('path', {}).inject(@operation.path.dup) do |path, (name, value)|
-        path.gsub("{#{name}}", value.to_s)
-      end
-    end
-
-    def full_path
-      query? ? "#{path}?#{query}" : path
-    end
-
-    def url
-      "#{@base_url}#{full_path}"
+      @request_context = request_context || build_request_context
     end
 
     def base_url(base_url)
-      self.class.new(
-        specification: @specification,
-        base_url: base_url,
-        operation: @operation,
-        params: @params
-      )
+      copy(base_url: base_url)
     end
 
     def operation(operation_name)
-      self.class.new(
-        specification: @specification,
-        base_url: @base_url,
-        operation: @specification.operation(operation_name),
-        params: @params
-      )
+      copy(operation: @specification.operation(operation_name))
     end
 
-    def params(**params)
-      self.class.new(
-        specification: @specification,
-        base_url: @base_url,
-        operation: @operation,
-        params: @params ? @params.merge(params) : params
-      )
+    def params(params)
+      params = params.transform_keys(&:to_s)
+      copy(params: @specification.build_grouped_params(@request_context.operation.node, params))
+    end
+
+    def headers(headers)
+      copy(headers: headers)
     end
 
     def execute
-      http_response = perform_request(url)
-      media_type = @specification.media_type(
-        @operation.node,
-        http_response.code,
-        http_response['Content-Type'].split(';').first
-      )
-      schema = @specification.schema(media_type.node)
-      ObjectBuilder.new(
-        media_type: media_type,
-        schema: schema,
-        http_response: http_response
-      ).call
+      build_object(build_request.perform)
     end
 
     private
 
-    def find_base_url
-      @specification.servers.first&.url
+    def build_request_context
+      RequestContext.new(base_url: @specification.default_base_url)
     end
 
-    def grouped_params
-      @grouped_params ||= @params ? build_grouped_params : {}
+    def copy(**properties)
+      self.class.new(
+        specification: @specification,
+        request_context: @request_context.copy(**properties)
+      )
     end
 
-    def build_grouped_params
-      @specification.dig(
-        *@operation.node, 'parameters'
-      ).each_with_object({}) do |attributes, grouped|
-        name = attributes['name']
-        next unless @params.key?(name)
-
-        group_name = attributes.fetch('in', '_')
-        grouped[group_name] ||= {}
-        grouped[group_name][name] = @params[name]
-      end
+    def build_request
+      Reynard::Http::Request.new(request_context: @request_context)
     end
 
-    def perform_request(url)
-      build_request(url).perform
-    end
-
-    def build_request(url)
-      Reynard::Http::Request.new(@operation.verb, url: url)
+    def build_object(http_response)
+      media_type = @specification.media_type(
+        @request_context.operation.node,
+        http_response.code,
+        http_response['Content-Type'].split(';').first
+      )
+      ObjectBuilder.new(
+        media_type: media_type,
+        schema: @specification.schema(media_type.node),
+        http_response: http_response
+      ).call
     end
   end
 end
--- test/reynard/context_test.rb
+++ test/reynard/context_test.rb
@@ -6,103 +6,67 @@ class Reynard
   class ContextTest < Reynard::Test
     def setup
       @specification = Specification.new(filename: fixture_file('openapi/simple.yml'))
+      @request_context = RequestContext.new(base_url: @specification.default_base_url)
+      @context = Context.new(specification: @specification, request_context: @request_context)
+    end
+
+    test 'does not have a verb without an operation' do
+      assert_nil @context.verb
+    end
+
+    test 'returns the verb for the operation' do
+      assert_equal 'get', @context.operation('searchBooks').verb
+    end
+
+    test 'does not have a path without an operation' do
+      assert_nil @context.path
+    end
+
+    test 'returns the path for the operation' do
+      assert_equal '/search/books', @context.operation('searchBooks').path
+    end
+
+    test 'does not have a full path without an operation' do
+      assert_nil @context.full_path
+    end
+
+    test 'returns the full path for the operation' do
+      assert_equal '/search/books', @context.operation('searchBooks').full_path
+    end
+
+    test 'returns its first server URL as its base URL by default' do
+      assert_equal('http://example.com/v1', @context.url)
     end
 
     test 'returns a specific URL as its base URL' do
       base_url = 'http://test.example.com/v1'
-      assert_equal(
-        base_url,
-        Reynard::Context.new(
-          specification: @specification,
-          base_url: base_url
-        ).url
-      )
+      assert_equal(base_url, @context.base_url(base_url).url)
     end
 
-    test 'returns its first server URL as its base URL' do
-      assert_equal(
-        'http://example.com/v1',
-        Reynard::Context.new(specification: @specification).url
-      )
-    end
-
-    test 'does not have a query without params' do
-      refute(
-        Reynard::Context.new(
-          specification: @specification,
-          operation: @specification.operation('searchBooks'),
-          params: nil
-        ).query?
-      )
-    end
-
-    test 'does not have a query when params are empty' do
-      refute(
-        Reynard::Context.new(
-          specification: @specification,
-          operation: @specification.operation('searchBooks'),
-          params: {}
-        ).query?
-      )
-    end
-
-    test 'does not have a query when params are not query params' do
-      refute(
-        Reynard::Context.new(
-          specification: @specification,
-          operation: @specification.operation('searchBooks'),
-          params: { name: 'Sama' }
-        ).query?
-      )
-    end
-
-    test 'has a query when there are query params' do
-      assert(
-        Reynard::Context.new(
-          specification: @specification,
-          operation: @specification.operation('searchBooks'),
-          params: { q: 'Sama' }
-        ).query?
-      )
+    test 'does not include query in the URL when no operation is selected' do
+      assert_nil URI(@context.url).query
     end
 
     test 'does not return a query when there are no params' do
-      assert_nil(
-        Reynard::Context.new(
-          specification: @specification,
-          operation: @specification.operation('searchBooks'),
-          params: nil
-        ).query
-      )
+      assert_nil URI(@context.operation('searchBooks').url).query
     end
 
     test 'builds a safe query when there are params' do
       assert_equal(
-        'q=%E2%9A%A1%EF%B8%8F',
-        Reynard::Context.new(
-          specification: @specification,
-          operation: @specification.operation('searchBooks'),
-          params: { q: '⚡️' }
-        ).query
+        'http://example.com/v1/search/books?q=%E2%9A%A1%EF%B8%8F',
+        @context.operation('searchBooks').params(q: '⚡️').url
+      )
+      assert_equal(
+        'http://example.com/v1/search/books?q=%22Lucene+Sky%22',
+        @context.operation('searchBooks').params(q: '"Lucene Sky"').url
       )
     end
 
     test 'builds url with parameter in path' do
-      context = Reynard::Context.new(
-        specification: @specification,
-        operation: @specification.operation('fetchBook'),
-        params: { id: 42 }
+      assert_equal(
+        'http://example.com/v1/books/42',
+        @context.operation('fetchBook').params(id: 42).url
       )
-      assert_equal 'http://example.com/v1/books/42', context.url
-    end
-
-    test 'builds url with parameter in query' do
-      context = Reynard::Context.new(
-        specification: @specification,
-        operation: @specification.operation('searchBooks'),
-        params: { q: '"Lucene Sky"' }
-      )
-      assert_equal 'http://example.com/v1/search/books?q=%22Lucene+Sky%22', context.url
     end
   end
 end
