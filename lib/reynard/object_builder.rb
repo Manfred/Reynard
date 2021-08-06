99c1bfc520a1627c51d84bf3dba1ffbb6f0a4298
Manfred Stienstra <manfred@fngtps.com>
2021-08-06 15:20:38 +0200

    Build object classes for collection items.

--- lib/reynard/object_builder.rb
+++ lib/reynard/object_builder.rb
@@ -11,28 +11,52 @@ class Reynard
       @http_response = http_response
     end
 
-    # Object.const_set(@media_type.schema_name, Class.new(Reynard::Model))
     def object_class
       if @media_type.schema_name
-        self.class.model_class(@media_type.schema_name)
+        self.class.model_class(@media_type.schema_name, @schema.object_type)
+      else
+        OpenStruct
+      end
+    end
+
+    def item_object_class
+      if @schema.item_schema_name
+        self.class.model_class(@schema.item_schema_name, 'object')
       else
         OpenStruct
       end
     end
 
     def call
-      case @media_type.media_type
-      when 'application/json'
-        object_class.new(MultiJson.load(@http_response.body))
+      if @schema.object_type == 'array'
+        array = object_class.new
+        data.each { |attributes| array << item_object_class.new(attributes) }
+        array
       else
-        FailedRequest.new
+        object_class.new(data)
       end
     end
 
-    def self.model_class(name)
+    def data
+      @data ||= MultiJson.load(@http_response.body)
+    end
+
+    def self.model_class(name, object_type)
+      model_class_get(name) || model_class_set(name, object_type)
+    end
+
+    def self.model_class_get(name)
       Reynard::Models.const_get(name)
     rescue NameError
-      Reynard::Models.const_set(name, Class.new(Reynard::Model))
+      nil
+    end
+
+    def self.model_class_set(name, object_type)
+      if object_type == 'array'
+        Reynard::Models.const_set(name, Class.new(Array))
+      else
+        Reynard::Models.const_set(name, Class.new(Reynard::Model))
+      end
     end
   end
 end
--- test/reynard/object_builder_test.rb
+++ test/reynard/object_builder_test.rb
@@ -8,7 +8,24 @@ class Reynard
       @specification = Specification.new(filename: fixture_file('openapi/simple.yml'))
     end
 
-    test 'build a singular record' do
+    test 'builds a collection' do
+      operation = @specification.operation('listBooks')
+      media_type = @specification.media_type(operation.node, '200', 'application/json')
+      schema = @specification.schema(media_type.node)
+      collection = Reynard::ObjectBuilder.new(
+        media_type: media_type,
+        schema: schema,
+        http_response: OpenStruct.new(body: '[{"id":42,"name":"Black Science"}]')
+      ).call
+      assert_kind_of(Reynard::ObjectBuilder.model_class('Books', 'array'), collection)
+
+      record = collection.first
+      assert_kind_of(Reynard::ObjectBuilder.model_class('Book', 'array'), record)
+      assert_equal 42, record.id
+      assert_equal 'Black Science', record.name
+    end
+
+    test 'builds a singular record' do
       operation = @specification.operation('fetchBook')
       media_type = @specification.media_type(operation.node, '200', 'application/json')
       schema = @specification.schema(media_type.node)
@@ -17,10 +34,7 @@ class Reynard
         schema: schema,
         http_response: OpenStruct.new(body: '{"id":42,"name":"Black Science"}')
       ).call
-      assert_kind_of(
-        Reynard::ObjectBuilder.model_class('Book'),
-        record
-      )
+      assert_kind_of(Reynard::ObjectBuilder.model_class('Book', 'object'), record)
       assert_equal 42, record.id
       assert_equal 'Black Science', record.name
     end
