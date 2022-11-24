3f76ba2dde0f3cd867fa274e9b7e86e96a9a6195
Manfred Stienstra <manfred@fngtps.com>
2022-11-24 08:31:50 +0100

    Change MediaType into just a node reference.
    We currently only use it as a bridge from the operation to
    the response schema. In the future we could still use it
    to warn about unexpected responses.

--- lib/reynard/media_type.rb
+++ lib/reynard/media_type.rb
@@ -1,21 +1,12 @@
 # frozen_string_literal: true
 
 class Reynard
-  # Holds node reference and schema name to a media type in the API specification.
+  # Holds node reference a media type in the API specification.
   class MediaType
-    attr_reader :node, :schema_name
+    attr_reader :node
 
-    def initialize(node:, schema_name:)
+    def initialize(node:)
       @node = node
-      @schema_name = schema_name
-    end
-
-    def media_type
-      @node[6]
-    end
-
-    def response_code
-      @node[4]
     end
   end
 end
