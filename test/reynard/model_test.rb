a96e56b668dfa7574fb3d88b655ca5f08233e215
Manfred Stienstra <manfred@fngtps.com>
2023-07-14 10:16:38 +0200

    Allow model attributes to be nil or false.
    Closes #34.

--- test/reynard/model_test.rb
+++ test/reynard/model_test.rb
@@ -5,11 +5,23 @@ require_relative '../test_helper'
 class Reynard
   class ModelTest < Reynard::Test
     def setup
-      @model = Model.new({ 'name' => 'James', 'age' => 12, 'address' => { 'zipcode' => '66232A' } })
+      @model = Model.new(
+        {
+          'name' => 'James',
+          'age' => 12,
+          'nickname' => nil,
+          'subscribed' => false,
+          'address' => {
+            'zipcode' => '66232A'
+          }
+        }
+      )
     end
 
     test 'responds to attributes' do
       assert @model.respond_to?(:name, private: false)
+      assert @model.respond_to?(:nickname, private: false)
+      assert @model.respond_to?(:subscribed, private: false)
     end
 
     test 'does not respond to random attributes' do
@@ -27,6 +39,8 @@ class Reynard
     test 'returns correct values for attributes' do
       assert_equal 'James', @model.name
       assert_equal 12, @model.age
+      assert_nil @model.nickname
+      assert_equal false, @model.subscribed
     end
 
     test 'allows access to attributes through [] accessor' do
@@ -42,11 +56,19 @@ class Reynard
     class Author < Model; end
 
     def setup
-      Author.inflector.snake_cases({ '1st-class' => 'first_class' })
+      Author.inflector.snake_cases(
+        {
+          '1st-class' => 'first_class',
+          '--allow' => 'allowed',
+          'startGRID' => 'start_grid'
+        }
+      )
       @author = Author.new(
         {
           'first_name' => 'James',
-          '1st-class' => 'false'
+          '1st-class' => 'false',
+          '--allow' => false,
+          'startGRID' => nil
         }
       )
     end
@@ -57,6 +79,8 @@ class Reynard
 
     test 'responds to mapped properties' do
       assert @author.respond_to?(:first_class)
+      assert @author.respond_to?(:allowed)
+      assert @author.respond_to?(:start_grid)
     end
 
     test 'returns correct values for regular attributes' do
@@ -65,6 +89,8 @@ class Reynard
 
     test 'returns correct values for mapped properties' do
       assert_equal 'false', @author.first_class
+      assert_equal false, @author.allowed
+      assert_nil @author.start_grid
     end
 
     test 'allows access to attributes through [] accessor' do
