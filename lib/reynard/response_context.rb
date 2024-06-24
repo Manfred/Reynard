48d1cb1d369c5170012536ac8d2be06977b4c287
Manfred Stienstra <manfred@fngtps.com>
2024-06-24 19:31:39 +0200

    Add deserializers property to response context.

--- lib/reynard/response_context.rb
+++ lib/reynard/response_context.rb
@@ -4,7 +4,13 @@ class Reynard
   # Value class for details about handling the response.
   ResponseContext = Struct.new(
     :logger,
+    :deserializers,
     keyword_init: true
   ) do
+    def copy(**properties)
+      copy = dup
+      properties.each { |attribute, value| copy.send("#{attribute}=", value) }
+      copy
+    end
   end
 end
--- test/reynard/response_context_test.rb
+++ test/reynard/response_context_test.rb
@@ -6,10 +6,26 @@ class Reynard
   class ResponseContextTest < Reynard::Test
     def setup
       @response_context = Reynard::ResponseContext.new
+      @logger = Logger.new('/dev/null')
     end
 
     test 'does not return a logger when it is not set' do
       assert_nil @response_context.logger
+    end
+
+    test 'does not have any serializers when none are set' do
+      assert_nil @response_context.deserializers
+    end
+
+    test 'can replace all properties when copying' do
+      properties = {
+        logger: @logger,
+        deserializers: Reynard.deserializers.dup
+      }
+      copy = @response_context.copy(**properties)
+      properties.each do |name, value|
+        assert_equal value, copy.send(name)
+      end
     end
   end
 end
